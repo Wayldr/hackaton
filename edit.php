0439950dd3681d3a5b55c45ada3be1ab476d4b49
Benjamin Bergeon <benjaminbergeon86@gmail.com>
2022-03-22 14:51:04 +0100

    ben

--- edit.php
+++ edit.php
@@ -1,33 +1,43 @@
 <?php
 // On démarre une session
 /* session_start(); */
+require_once('connect.php');
+$sqlthemes = 'SELECT id_themes,name FROM themes';
 
-if($_POST){
-    if(isset($_POST['id']) && !empty($_POST['id'])
-    && isset($_POST['produit']) && !empty($_POST['produit'])
-    && isset($_POST['prix']) && !empty($_POST['prix'])
-    && isset($_POST['nombre']) && !empty($_POST['nombre'])){
+// On prépare la requête
+$querythemes = $db->prepare($sqlthemes);
+
+// On exécute la requête
+$querythemes->execute();
+
+// On stocke le résultat dans un tableau associatif
+$resultat = $querythemes->fetchAll(PDO::FETCH_ASSOC);
+if($_GET){
+    if(isset($_GET['id']) && !empty($_GET['id'])
+    && isset($_GET['question']) && !empty($_GET['question'])
+    && isset($_GET['reponse']) && !empty($_GET['reponse'])
+    && isset($_GET['id_themes']) && !empty($_GET['id_themes'])){
         // On inclut la connexion à la base
         require_once('connect.php');
 
         // On nettoie les données envoyées
-        $id = strip_tags($_POST['id']);
-        $produit = strip_tags($_POST['produit']);
-        $prix = strip_tags($_POST['prix']);
-        $nombre = strip_tags($_POST['nombre']);
+        $id = strip_tags($_GET['id']);
+        $question = strip_tags($_GET['question']);
+        $reponse = strip_tags($_GET['reponse']);
+        $id_themes = strip_tags($_GET['id_themes']);
 
-        $sql = 'UPDATE `liste` SET `produit`=:produit, `prix`=:prix, `nombre`=:nombre WHERE `id`=:id;';
+        $sql = 'UPDATE `la_faq` SET `question`=:question, `reponse`=:reponse, `id_themes`=:id_themes WHERE `id`=:id;';
 
         $query = $db->prepare($sql);
 
         $query->bindValue(':id', $id, PDO::PARAM_INT);
-        $query->bindValue(':produit', $produit, PDO::PARAM_STR);
-        $query->bindValue(':prix', $prix, PDO::PARAM_STR);
-        $query->bindValue(':nombre', $nombre, PDO::PARAM_INT);
+        $query->bindValue(':question', $question, PDO::PARAM_STR);
+        $query->bindValue(':reponse', $reponse, PDO::PARAM_STR);
+        $query->bindValue(':id_themes', $id_themes, PDO::PARAM_INT);
 
         $query->execute();
 
-        $_SESSION['message'] = "Produit modifié";
+        $_SESSION['message'] = "question modifié";
         require_once('close.php');
 
         header('Location: index.php');
@@ -43,7 +53,7 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
     // On nettoie l'id envoyé
     $id = strip_tags($_GET['id']);
 
-    $sql = 'SELECT * FROM `liste` WHERE `id` = :id;';
+    $sql = 'SELECT * FROM `la_faq` WHERE `id` = :id;';
 
     // On prépare la requête
     $query = $db->prepare($sql);
@@ -54,11 +64,11 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
     // On exécute la requête
     $query->execute();
 
-    // On récupère le produit
-    $produit = $query->fetch();
+    // On récupère le question
+    $result = $query->fetch();
 
-    // On vérifie si le produit existe
-    if(!$produit){
+    // On vérifie si le question existe
+    if(!$question){
         $_SESSION['erreur'] = "Cet id n'existe pas";
         header('Location: index.php');
     }
@@ -80,20 +90,20 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
                         $_SESSION['erreur'] = "";
                     }
                 ?>
-                <h1>Modifier un produit</h1>
+                <h1>Modifier un question</h1>
                 <form method="get">
                     <div class="form-group">
                         <label for="id_themes">Themes</label>
                         <select class="form-select" id="inputGroupSelect01" name="id_themes">
                         <?php 
-                        foreach($resultat as $produit){
-                            if($produit['id_themes']==0){
+                        foreach($resultat as $question){
+                            if($question['id_themes']==0){
                         ?>
-                            <option selected value="<?php echo $produit['id_themes']?>" name="id_themes"><?php echo $produit['name'];?></option>
+                            <option selected value="<?php echo $question['id_themes']?>" name="id_themes"><?php echo $question['name'];?></option>
                             <?php
                             }else{
                         ?>
-                            <option value="<?php echo $produit['id_themes']?>" name="id_themes"><?php echo $produit['name'];?></option>
+                            <option value="<?php echo $question['id_themes']?>" name="id_themes"><?php echo $question['name'];?></option>
                             
                         <?php
                             }
@@ -103,7 +113,7 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
                     </div>
                     <div class="form-group">
                         <label for="question">question</label>
-                        <input type="text" id="question" name="question" class="form-control">
+                        <input type="text" id="question" name="question" class="form-control" value="">
                     </div>
                     <div class="form-group">
                         <label for="reponse">reponse</label>
@@ -115,4 +125,4 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
             </section>
         </div>
     </main>
-<?php require_once('footer.php');?>
+<?php require_once('../footer.php');?>
--- index.php
+++ index.php
@@ -1,5 +1,78 @@
+<?php
+// On démarre une session
+/* session_start(); */
+
+// On inclut la connexion à la base
+require_once('connect.php');
+
+$sql = 'SELECT id,name,question,reponse FROM themes,la_faq WHERE la_faq.id_themes=themes.id_themes';
+
+// On prépare la requête
+$query = $db->prepare($sql);
+
+// On exécute la requête
+$query->execute();
+
+// On stocke le résultat dans un tableau associatif
+$result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+require_once('close.php');
+?>
 <?php require_once('header.php');?>
-    <main>
-        <?php require_once('php-crud-master/index.php');?>
+    <main class="container">
+        <div class="row">
+            <section class="col-12">
+            <?php
+            if (isAdmin()){ 
+            ?>
+                <a href="add.php" class="btn btn-primary">Ajouter un produit</a>
+            <?php
+            }
+            ?>
+            <?php
+                    if(!empty($_SESSION['erreur'])){
+                        echo '<div class="alert alert-danger" role="alert">
+                                '. $_SESSION['erreur'].'
+                            </div>';
+                        $_SESSION['erreur'] = "";
+                    }
+                ?>
+                <?php
+                    if(!empty($_SESSION['message'])){
+                        echo '<div class="alert alert-success" role="alert">
+                                '. $_SESSION['message'].'
+                            </div>';
+                        $_SESSION['message'] = "";
+                    }
+                    ?>
+                
+                <div class="accordion" id="accordionExample">
+                <?php foreach($result as $produit){
+                ?>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $produit['id']?>" aria-expanded="true" aria-controls="collapseOne">
+                            <?php  echo $produit['question']?>
+                        </button>
+                        </h2>
+                        <div id="collapse<?php echo $produit['id']?>" class="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <?php echo $produit['reponse']?>
+                            <?php
+                                /* if (isAdmin()){  */
+                            ?>
+                            <a href="edit.php?id=<?php echo $produit['id']?>" class="btn btn-primary">Modifer</a>
+                            <?php
+                               /*  } */
+                            ?>
+                        </div>
+                        </div>
+                    </div>
+                    <?php 
+                }
+                ?>
+                </div>
+            </section>
+        </div>
     </main>
 <?php require_once('footer.php');?>
