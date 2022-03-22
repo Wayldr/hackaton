a43a18181be3264af87f4e198111b0ae83e69d03
Wayldr <steinerjules@gmail.com>
2022-03-22 11:23:09 +0100

    Notice d'utilisation

--- session_admin.php
+++ session_admin.php
@@ -9,6 +9,17 @@ function isAdmin(){
     }
     return true;
 }
+
+//NOTICE D'UTILISATION 
+
+/* Pour verifier si l'admin est connécté utliisé le test ci-dessous:
+
+if (isAdmin()){ //test si l'admin est connecté
+    
+    //ce qu'il faut faire
+} 
+
+*/
 ?>
 
 
