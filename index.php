6ba27f2fd2acbbad41a19f44aa0c45539af220de
Wayldr <steinerjules@gmail.com>
2022-03-22 11:18:46 +0100

    mise en place de la fonction isAdmin

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <?php
-/* Tout ce qui se passe ici sera afficher sera afficher quelque soit le profil*/
-
 session_start();
-if (!isset($_SESSION["profil"])){
-    return;
+function isAdmin(){
+    if (!isset($_SESSION["profil"])){
+        return false;
+    }
+    if ($_SESSION["profil"]!="admin"){
+        return false;
+    }
+    return true;
 }
-if ($_SESSION["profil"]!="admin") {
-    return;
-}
-/* Tout ce qui se passe ici sera afficher uniquement si on est connecté en tant qu'admin
- */
-
 ?>
 
 
